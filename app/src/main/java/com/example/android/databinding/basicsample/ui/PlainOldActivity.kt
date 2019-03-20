4ffe6c70b9f519796965713e6f8a7988d7b6fc42
Chris Rodriguez <cerdevelopers@gmail.com>
2019-03-20 00:37:42 -0500

    UPDATE: PlainOldActivity -  • Removed unecessary code plain_activity.xml  • likes texview now references likes from ViewModel

--- app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
@@ -17,19 +17,10 @@
 package com.example.android.databinding.basicsample.ui
 
 import android.arch.lifecycle.ViewModelProviders
-import android.content.Context
-import android.content.res.ColorStateList
 import android.databinding.DataBindingUtil
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
-import android.support.v4.widget.ImageViewCompat
 import android.support.v7.app.AppCompatActivity
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
 import com.example.android.databinding.basicsample.R
-import com.example.android.databinding.basicsample.data.Popularity
 import com.example.android.databinding.basicsample.data.SimpleViewModel
 import com.example.android.databinding.basicsample.databinding.PlainActivityBinding
 
@@ -52,7 +43,7 @@ class PlainOldActivity : AppCompatActivity() {
 
         // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
         //removed update name method since its not needed with binding
-        updateLikes()
+
     }
 
     /**
@@ -72,40 +63,7 @@ class PlainOldActivity : AppCompatActivity() {
      * - It has untestable logic
      * - It's updating two views when called even if they're not changing
      */
-    private fun updateLikes() {
-        findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
-        findViewById<ProgressBar>(R.id.progressBar).progress =
-            (viewModel.likes * 100 / 5).coerceAtMost(100)
-        val image = findViewById<ImageView>(R.id.imageView)
+    //don't need udate likes function since its called from ViewModel now
 
-        val color = getAssociatedColor(viewModel.popularity, this)
 
-        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
-
-        image.setImageDrawable(getDrawablePopularity(viewModel.popularity, this))
-    }
-
-    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
-        return when (popularity) {
-            Popularity.NORMAL -> context.theme.obtainStyledAttributes(
-                intArrayOf(android.R.attr.colorForeground)
-            ).getColor(0, 0x000000)
-            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
-            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
-        }
-    }
-
-    private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
-        return when (popularity) {
-            Popularity.NORMAL -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
-            }
-            Popularity.POPULAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-            Popularity.STAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-        }
-    }
 }
